7a456eb5b0a10aa5c3e5c2ce4dc61a7fc77d1649
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-19 22:38:19 +0400

    api completed

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/productRoute.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); 
+
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
+});
