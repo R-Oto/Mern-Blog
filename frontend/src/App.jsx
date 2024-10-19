1f5243595898786b52ccd7a214e7e91da39dd889
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-19 22:51:36 +0400

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {Box, Button} from '@chakra-ui/react'
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <div className=''>
-      
+      <Box minH={"100vh"}>
+        <Routes>
+          <Route path='/' element={<Home></Home>} ></Route>
+          <Route path='/create' element={<Create></Create>} ></Route>
+        </Routes>
+      </Box>
     </div>
   )
 }
